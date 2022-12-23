f61c97d6e4ea00cef32c16d50fb3c929a3aed97c
Roben Kleene <contact@robenkleene.com>
2022-12-23 02:04:46 +0000

    Work on Ruby convert

--- export_code_snippets_to_yasnippet.rb
+++ export_code_snippets_to_yasnippet.rb
@@ -1,2 +1,17 @@
 #!/usr/bin/env ruby
 
+require 'json'
+JSON.parse(json_string)
+
+code_snippets_path = "#{Dir.home}/.config/Code/User/snippets"
+unless directory_exists?(code_snippets_path)
+  STDERR.puts "Code snippets dir #{code_snippets_path} not found"
+  exit 1
+end
+
+yasnippets_snippets_path = "#{Dir.home}/.config/Code/User/snippets"
+unless directory_exists?(yasnippets_snippets_path)
+  STDERR.puts "YASnippets snippets dir #{yasnippets_snippets_path} not found"
+  exit 1
+end
+

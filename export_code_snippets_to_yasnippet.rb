23e6569d1ecdd422aec788e758b037367a703e21
Roben Kleene <contact@robenkleene.com>
2022-12-23 02:18:13 +0000

    Iterate through filenames

--- export_code_snippets_to_yasnippet.rb
+++ export_code_snippets_to_yasnippet.rb
@@ -1,20 +1,20 @@
 #!/usr/bin/env ruby
 
 require 'json'
-JSON.parse(json_string)
 
 code_snippets_path = "#{Dir.home}/.config/Code/User/snippets"
-unless directory_exists?(code_snippets_path)
+unless File.directory?(code_snippets_path)
   STDERR.puts "Code snippets dir #{code_snippets_path} not found"
   exit 1
 end
 
 yasnippets_snippets_path = "#{Dir.home}/.config/Code/User/snippets"
-unless directory_exists?(yasnippets_snippets_path)
+unless File.directory?(yasnippets_snippets_path)
   STDERR.puts "YASnippets snippets dir #{yasnippets_snippets_path} not found"
   exit 1
 end
 
-Dir.foreach("#{code_snippets_path}/*.json") do |filename|
+Dir.glob("#{code_snippets_path}/*.json") do |filename|
   puts "filename = " + filename
+  # JSON.parse(json_string)
 end

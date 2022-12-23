c0a122fd847761983606fa7be2b4336bdd67d76f
Roben Kleene <contact@robenkleene.com>
2022-12-23 15:15:12 +0000

    Work on snippets

--- export_code_snippets_to_yasnippet.rb
+++ export_code_snippets_to_yasnippet.rb
@@ -1,6 +1,9 @@
 #!/usr/bin/env ruby
 
 require 'json'
+require 'pathname'
+
+filename_to_mode = { "objective-c" => "objc" }
 
 code_snippets_path = "#{Dir.home}/.config/Code/User/snippets"
 unless File.directory?(code_snippets_path)
@@ -14,10 +17,22 @@ unless File.directory?(yasnippets_snippets_path)
   exit 1
 end
 
-Dir.glob("#{code_snippets_path}/*.json") do |filename|
-  contents = File.read(filename)
+Dir.glob("#{code_snippets_path}/*.json") do |file_path|
+  contents = File.read(file_path)
   json = JSON.parse(contents)
-  json.each do | key, body |
-    puts "#{key}: $#{body}"
+  filename = File.basename(file_path, File.extname(file_path))
+  mode = filename_to_mode[filename].nil? ? filename : filename_to_mode[filename]
+  dest_dir = File.join(yasnippets_snippets_path, "#{filename}-mode")
+
+  json.each do | key, hash |
+    prefix = hash['prefix']
+    body = hash['body']
+    template = "# key: #{prefix}
+# name: #{prefix}
+# --
+#{body}"
+    # puts "#{template}\n\n"
+    dest_path = File.join(dest_dir, prefix)
+    puts dest_path
   end
 end
